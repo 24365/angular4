a7fdd5a8ba654f4e2bcbaf3d27cafce5be6c5fb7
24365 <me@zengxingqi.com>
2017-04-07 10:05:33 +0800

    更新

--- angular4/src/app/product-detail/product-detail.component.ts
+++ angular4/src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {Product, ProductService} from "../shared/product.service";
+import {ActivatedRoute, Data} from "@angular/router";
+import {Product, ProductService,Comment} from "../shared/product.service";
 
 @Component({
   selector: 'app-product-detail',
@@ -12,6 +12,8 @@ export class ProductDetailComponent implements OnInit {
   comments:Component[];
   newRating:number=5;
   newComment:string="";
+  isCommentHidden:boolean=true;
+
   constructor(
     private routeInfo:ActivatedRoute,
     private productService:ProductService
@@ -22,7 +24,15 @@ export class ProductDetailComponent implements OnInit {
     this.product =this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
   }
-
+  addComment(){
+    let comment = new Comment(0,this.product.id,new Date().toISOString(),"someone",this.newRating,this.newComment);
+    this.comments.unshift(comment);
+    // let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    // this.product.rating= sum / this.comments.length;
+    this.newComment=null;
+    this.newRating=5;
+    this.isCommentHidden=true;
+  }
 }
 
 
